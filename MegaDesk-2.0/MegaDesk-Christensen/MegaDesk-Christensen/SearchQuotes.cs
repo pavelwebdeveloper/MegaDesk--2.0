ef7b3dd4c29f9644ade9e906a81ba63e5c040302
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2022-02-06 00:01:18 +0200

    added search and viewall quotes files

---
+++ MegaDesk-2.0/MegaDesk-Christensen/MegaDesk-Christensen/SearchQuotes.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MegaDesk_Christensen
+{
+    public partial class SearchQuotes : Form
+    {
+        public string desktop_Material;
+        
+        List<DeskQuote> foundDeskQuotes;
+        public SearchQuotes()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            ComboBox desktopMaterialInput = sender as ComboBox;
+            desktop_Material = desktopMaterialInput.SelectedValue.ToString();
+
+            //MessageBox.Show($"{desktop_Material}");
+
+            foundDeskQuotes = new List<DeskQuote>();
+
+            //MessageBox.Show($"{Program.deskQuoteList.Count()}");
+
+            foreach (DeskQuote deskQuote in Program.deskQuoteList)
+            {
+                //MessageBox.Show($"{deskQuote}");
+                if (deskQuote.Desktop_Material == desktop_Material)
+                {
+                    foundDeskQuotes.Add(deskQuote);
+                }
+            }
+
+            
+            DataGridView.DataSource = foundDeskQuotes;
+
+
+
+        }
+
+        private void comboBox1_DropDown(object sender, EventArgs e)
+        {
+            comboBox1.DataSource = Enum.GetValues(typeof(Desk.DesktopMaterial));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            MainMenu MM = new MainMenu();
+            MM.ShowDialog();
+            this.Close();
+        }
+
+        private void SearchQuotes_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MegaDesk-2.0/MegaDesk-Christensen/MegaDesk-Christensen/ViewAllQuotes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MegaDesk_Christensen
+{
+    public partial class ViewAllQuotes : Form
+    {
+        public ViewAllQuotes()
+        {
+            InitializeComponent();
+            dataGridView1.DataSource = Program.deskQuoteList;
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            MainMenu MM = new MainMenu();
+            MM.ShowDialog();
+            this.Close();
+        }
+
+        private void ViewAllQuotes_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
