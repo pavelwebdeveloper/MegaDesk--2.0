259e7878768cc30dd5a188e547f8c9c22ce25d08
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2022-02-06 00:04:38 +0200

    added program file

---
+++ MegaDesk-2.0/MegaDesk-Christensen/MegaDesk-Christensen/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MegaDesk_Christensen
+{
+    internal static class Program
+    {
+        public static List<DeskQuote> deskQuoteList;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //MessageBox.Show("The program has started");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            deskQuoteList = DeskQuote.Load("desk_quote_list.json");
+
+            //MessageBox.Show($"{deskQuoteList}");
+            
+            if (deskQuoteList == null) 
+            {
+                deskQuoteList = new List<DeskQuote>();
+            }
+
+            Application.Run(new MainMenu());
+        }
+    }
+}
